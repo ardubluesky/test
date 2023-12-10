84d4148657564058a5d5826aafe1351fd0313330
ardubluesky <akashneel.bhattacharya@outlook.com>
2023-12-11 00:59:10 +0530

    initial commit

---
+++ src/functions.js
@@ -0,0 +1,18 @@
+var greetInputEl = document.getElementById("greet-input");
+var greetMsgEl = document.getElementById("greet-msg");
+
+async function greet() {
+  var name = greetInputEl.value;
+  
+  greetMsgEl.textContent = "hello there "+name+"! welcome to this app.";
+
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
